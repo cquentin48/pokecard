5b054f4ffcddf07e41f7ecad031cacb3e26d82c3
Quentin CHAPEL <chapelquen@gmail.com>
2018-10-15 17:05:27 +0200

    gitflow-feature-stash: test-branche

--- index.php
+++ index.php
@@ -1,9 +1,47 @@
 <?php
   echo "Hello World !";
-  if(file_exists("test.php")){
-    echo "Fichier existant!";
-  }else{
-    echo "Fichier inconnu!";
-  }
-  include "test.php";
+
+  <?php
+    function CallAPI($method, $url, $data = false)
+    {
+        $curl = curl_init();
+
+        switch ($method)
+        {
+            case "POST":
+                curl_setopt($curl, CURLOPT_POST, 1);
+
+                if ($data)
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+                break;
+            case "PUT":
+                curl_setopt($curl, CURLOPT_PUT, 1);
+                break;
+            default:
+                if ($data)
+                    $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
+
+        // Optional Authentication:
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $result;
+    }
+
+    $method = "POST";
+    $url = "https://pokeapi.co/api/v2/pokedex/1/";
+    $returnData = CallAPI($method, $url);
+    echo "<pre>";
+      print_r($returnData);
+    echo "</pre>";
+    echo "<br/>";
+  ?>
 ?>
