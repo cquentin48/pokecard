53ca4104eff0862108b2f24553049f0f16e43e8b
Noé Colin <noe_colin@yahoo.fr>
2019-01-23 13:34:18 +0100

    map set. location in progress

--- Android/app/src/main/java/com/pokeapi/lpiem/pokeapiandroid/View/MapFragment.kt
+++ Android/app/src/main/java/com/pokeapi/lpiem/pokeapiandroid/View/MapFragment.kt
@@ -16,63 +16,51 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
+import com.pokeapi.lpiem.pokeapiandroid.R
 
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [MapFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [MapFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class MapFragment : Fragment() {
+    private var ARG_PARAM1:String = "latitude"
+    private var ARG_PARAM2 :String = "longitude"
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var latitude: Double = 0.toDouble()
+    private var longitude: Double = 0.toDouble()
     private var listener: OnFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            latitude = it.getDouble(ARG_PARAM1)
+            longitude = it.getDouble(ARG_PARAM2)
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        val rootView = inflater.inflate(com.pokeapi.lpiem.pokeapiandroid.R.layout.fragment_map, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_map, container, false)
 
-        val mapFragment = childFragmentManager.findFragmentById(com.pokeapi.lpiem.pokeapiandroid.R.id.frg) as SupportMapFragment?  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+        val mapFragment = childFragmentManager.findFragmentById(R.id.frg) as SupportMapFragment?  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
         mapFragment!!.getMapAsync { mMap ->
             mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
 
             mMap.clear() //clear old markers
 
-            val googlePlex = CameraPosition.builder()
+          /*  val googlePlex = CameraPosition.builder()
                     .target(LatLng(37.4219999, -122.0862462))
                     .zoom(10f)
                     .bearing(0f)
                     .tilt(45f)
-                    .build()
+                    .build()*/
 
-            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null)
+            //mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null)
 
             mMap.addMarker(MarkerOptions()
-                    .position(LatLng(37.4219999, -122.0862462))
-                    .title("Spider Man"))
+                    .position(LatLng(latitude, longitude))
+                    .title("Moi"))
         }
         // Inflate the layout for this fragment
-        return rootView;
+        return rootView
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -120,12 +108,12 @@ class MapFragment : Fragment() {
          * @return A new instance of fragment MapFragment.
          */
         // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+
+        fun newInstance(latitude: Double, longitude: Double) =
                 MapFragment().apply {
                     arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
+                        putDouble(ARG_PARAM1, latitude)
+                        putDouble(ARG_PARAM2, longitude)
                     }
                 }
     }
