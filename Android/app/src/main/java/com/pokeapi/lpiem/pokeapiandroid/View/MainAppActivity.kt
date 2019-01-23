2c8600e47549656d8e2936b9f16c4d2150d009d2
Quentin CHAPEL <chapelquen@gmail.com>
2019-01-23 08:10:26 +0100

    [Feat]Basic navigation drawer
    Adding basic navigation drawer to main application.

--- Android/app/src/main/java/com/pokeapi/lpiem/pokeapiandroid/View/MainAppActivity.kt
+++ Android/app/src/main/java/com/pokeapi/lpiem/pokeapiandroid/View/MainAppActivity.kt
@@ -1,8 +1,13 @@
 package com.pokeapi.lpiem.pokeapiandroid.View
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.pokeapi.lpiem.pokeapiandroid.Model.Pokemon.Model.PokemonData
 import com.pokeapi.lpiem.pokeapiandroid.Model.Pokemon.Retrofit.PokemonRetrofit
 import com.pokeapi.lpiem.pokeapiandroid.Model.Pokemon.Retrofit.Species
@@ -22,7 +27,30 @@ class MainAppActivity : AppCompatActivity(), InterfaceCallBackController<Any>{
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_app)
         mDrawerLayout = findViewById(R.id.drawer_layout)
+
         //singleton!!.getPokeApiInfos(this)
+
+        val navigationView: NavigationView = findViewById(R.id.navigationView)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers()
+
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+
+            true
+        }
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_settings_black_24dp)
+        }
+
     }
 
     override fun addPokemonSpecies(i: Int, s: Species) {
@@ -38,5 +66,15 @@ class MainAppActivity : AppCompatActivity(), InterfaceCallBackController<Any>{
                 p.species!!,
                 p.typeList!!.toMutableList(),
                 p.id))
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
