5c2e652b9de6727d29f6cd82a797c36ce4a230dd
Quentin CHAPEL <chapelquen@gmail.com>
2019-01-23 06:57:23 +0100

    [Feat]Work done at 7:00 am
    Trying to fix navigation drawer.

--- Android/app/src/main/java/com/pokeapi/lpiem/pokeapiandroid/View/MainAppActivity.kt
+++ Android/app/src/main/java/com/pokeapi/lpiem/pokeapiandroid/View/MainAppActivity.kt
@@ -1,14 +1,8 @@
 package com.pokeapi.lpiem.pokeapiandroid.View
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.appcompat.app.ActionBar
-import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
 import com.pokeapi.lpiem.pokeapiandroid.Model.Pokemon.Model.PokemonData
 import com.pokeapi.lpiem.pokeapiandroid.Model.Pokemon.Retrofit.PokemonRetrofit
 import com.pokeapi.lpiem.pokeapiandroid.Model.Pokemon.Retrofit.Species
@@ -17,7 +11,7 @@ import com.pokeapi.lpiem.pokeapiandroid.Provider.Singleton.AppProviderSingleton
 import com.pokeapi.lpiem.pokeapiandroid.R
 
 
-class MainAppActivity : AppCompatActivity(), InterfaceCallBackController<Any>,NavigationView.OnNavigationItemSelectedListener {
+class MainAppActivity : AppCompatActivity(), InterfaceCallBackController<Any>{
     private var singleton: AppProviderSingleton?= AppProviderSingleton.getInstance()
     private lateinit var mDrawerLayout: DrawerLayout
 
@@ -28,21 +22,7 @@ class MainAppActivity : AppCompatActivity(), InterfaceCallBackController<Any>,Na
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_app)
         mDrawerLayout = findViewById(R.id.drawer_layout)
-
-        val navigationView: NavigationView = findViewById(R.id.nav_view)
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            // set item as selected to persist highlight
-            menuItem.isChecked = true
-            // close drawer when item is tapped
-            mDrawerLayout.closeDrawers()
-
-            // Add code here to update the UI based on the item selected
-            // For example, swap UI fragments here
-
-            true
-        }
-        singleton!!.getPokeApiInfos(this)
-        //initNavigationDrawer()
+        //singleton!!.getPokeApiInfos(this)
     }
 
     override fun addPokemonSpecies(i: Int, s: Species) {
@@ -58,38 +38,5 @@ class MainAppActivity : AppCompatActivity(), InterfaceCallBackController<Any>,Na
                 p.species!!,
                 p.typeList!!.toMutableList(),
                 p.id))
-    }
-
-    private fun initNavigationDrawer(){
-
-        val navigationView: NavigationView = findViewById(R.id.nav_view)
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            // set item as selected to persist highlight
-            menuItem.isChecked = true
-            // close drawer when item is tapped
-            mDrawerLayout.closeDrawers()
-
-            // Add code here to update the UI based on the item selected
-            // For example, swap UI fragments here
-
-            true
-        }
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        val id = item.itemId
-
-        when(id){
-           R.id.navPokeMap->Toast.makeText(this,"Pas encore implémenté",Toast.LENGTH_LONG).show()
-           R.id.navPokedex->startActivity(Intent(this,PokedexPokemonView::class.java))
-           R.id.navProfile->Toast.makeText(this,"Pas encore implémenté",Toast.LENGTH_LONG).show()
-           R.id.navExchange-> Toast.makeText(this,"Pas encore implémenté",Toast.LENGTH_LONG).show()
-           R.id.navSignOut->startActivity(Intent(this,MainActivity::class.java))
-        }
-
-        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
-        drawer.closeDrawer(GravityCompat.START)
-        return true
     }
 }
